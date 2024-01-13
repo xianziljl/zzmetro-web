2375608ca9004aaf1393c99d43f22dcc24e35d7f
zhuxianyong <xianziycg@gmail.com>
2024-01-13 22:52:44 +0800

    调整目录结构

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,26 @@ import handlebars from 'vite-plugin-handlebars';
 
 export default {
   base: './',
+  root: path.resolve(__dirname, 'src'),
   plugins: [
     UnoCSS(),
-    handlebars({ partialDirectory: path.resolve(__dirname, 'partials') }),
+    handlebars({ partialDirectory: path.resolve(__dirname, 'src/partials') }),
     legacy({ targets: 'firefox 52' }),
   ],
   build: {
     rollupOptions: {
       input: {
-        'index': path.resolve(__dirname, 'index.html'),
-        'home': path.resolve(__dirname, 'home/index.html'),
+        'index': path.resolve(__dirname, 'src/index.html'),
+        'about': path.resolve(__dirname, 'src/about/index.html'),
+        'home': path.resolve(__dirname, 'src/home/index.html'),
       },
       output: {
         chunkFileNames: "static/js/[name].js",
         entryFileNames: "static/js/[name].js",
         assetFileNames: "static/[ext]/[name].[ext]",
       },
-    }
+    },
+    outDir: '../dist',
+    emptyOutDir: true,
   }
 };
